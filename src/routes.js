b3c74f1730e274221477f7077dc5605395fa17f7
zizy97 <supuntharuka9749@gmail.com>
2022-06-07 12:43:14 +0530

    add dashboard to app

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,26 @@ import { Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import CustomDrawer from "./components/CustomDrawer";
 import TestCase from "./test/TestCase";
+import Dashboard from "../src/components/Dashboard-main";
 
 const routes = (isAuthenticated) => [
   {
     path: "",
-    element: <Home/>,
+    element: <Home />,
   },
-  {path: "test",element: <TestCase/>},
+  { path: "test", element: <TestCase /> },
   {
     path: "admin",
-    element: isAuthenticated?<CustomDrawer/>:<Navigate to="/"/>,
+    element: isAuthenticated ? <CustomDrawer /> : <Navigate to="/" />,
+    children: [
+      { path: "", element: <h1>This is space to new component</h1> },
+      { path: "users", element: <h1>User List component</h1> },
+      { path: "*", element: <Navigate to="/404" /> },
+    ],
+  },
+  {
+    path: "vo",
+    element: isAuthenticated ? <Dashboard /> : <Navigate to="/" />,
     children: [
       { path: "", element: <h1>This is space to new component</h1> },
       { path: "users", element: <h1>User List component</h1> },
