33aaca1bae6054af6162d23b1951e0fb0a3d15f9
zizy97 <supuntharuka9749@gmail.com>
2022-06-08 05:32:59 +0530

    fix bug

--- src/api/socket/socket.js
+++ src/api/socket/socket.js
@@ -1,38 +1,38 @@
-import Stomp from "stompjs";
-import SockJS from "sockjs-client";
+// import Stomp from "stompjs";
+// import SockJS from "sockjs-client";
 
-var stompClient=null;
+// var stompClient=null;
 
-export default class SocketBuilder {
+// export default class SocketBuilder {
 
-  stompClient = null;
+//   stompClient = null;
 
-  constructor(){
-    var socket = new SockJS("http://localhost:8080/stomp-endpoint");
-    this.stompClient = Stomp.over(socket);
-  }
+//   constructor(){
+//     var socket = new SockJS("http://localhost:8080/stomp-endpoint");
+//     this.stompClient = Stomp.over(socket);
+//   }
 
-  connect = async (setConnected, setMessage) => {
-    var socket = new SockJS("http://localhost:8080/stomp-endpoint");
-    stompClient = Stomp.over(socket);
-    stompClient.connect({"user":"username"}, function (frame) {
-      setConnected(true);
-      console.log("Connected: " + frame);
-    stompClient.subscribe("/topic/greetings", function (greeting) {
-        setMessage(JSON.parse(greeting.body));
-      });
-    });
-    this.stompClient=stompClient;
-  };
+//   connect = async (setConnected, setMessage) => {
+//     var socket = new SockJS("http://localhost:8080/stomp-endpoint");
+//     stompClient = Stomp.over(socket);
+//     stompClient.connect({"user":"username"}, function (frame) {
+//       setConnected(true);
+//       console.log("Connected: " + frame);
+//     stompClient.subscribe("/topic/greetings", function (greeting) {
+//         setMessage(JSON.parse(greeting.body));
+//       });
+//     });
+//     this.stompClient=stompClient;
+//   };
 
-  disconnect() {
-    if (this.stompClient !== null) {
-      this.stompClient.disconnect();
-    }
-    console.log("Disconnected");
-  }
+//   disconnect() {
+//     if (this.stompClient !== null) {
+//       this.stompClient.disconnect();
+//     }
+//     console.log("Disconnected");
+//   }
 
-  sendName(name) {
-    this.stompClient.send("/app/hello", {}, JSON.stringify({ name: name }));
-  }
-}
+//   sendName(name) {
+//     this.stompClient.send("/app/hello", {}, JSON.stringify({ name: name }));
+//   }
+// }
